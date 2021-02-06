a1a11e59a4a3194f39ca07784e58817700aa6f82
Seid <seidshemsu486@yahoo.com>
2021-02-07 00:14:29 +0300

    approval adapter ga new yekomkut

--- app/src/main/java/com/izhar/resto/Approval.java
+++ app/src/main/java/com/izhar/resto/Approval.java
@@ -1,17 +1,31 @@
 package com.izhar.resto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.izhar.resto.R;
 
 public class Approval extends AppCompatActivity {
 
+    RecyclerView recycle;
+    Button approve;
+    LottieAnimationView loader;
+    TextView total;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_approval);
-
+        approve = findViewById(R.id.approve);
+        loader = findViewById(R.id.loader);
+        total = findViewById(R.id.total_price);
+        recycle = findViewById(R.id.recycle);
+        recycle.setHasFixedSize(true);
+        recycle.setLayoutManager(new LinearLayoutManager(this));
     }
 }
